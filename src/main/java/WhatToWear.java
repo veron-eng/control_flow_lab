1c64623cec4af49f2afba0ca5ce112bc7e89a0c2
Veron Sundaram <veronsundaram13@gmail.com>
2022-08-11 23:17:29 +0100

    complete extention- what to wear

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class WhatToWear {
 
     public static void main(String[] args) {
+
+        System.out.println("Enter the weather (Rain,Sun,Snow,Cloudy)");
+
+        Scanner weather = new Scanner(System.in);
+
+        String w = weather.nextLine();
+
+        System.out.println("Enter the average temperature (0-30)");
+
+        Scanner temperature = new Scanner(System.in);
+
+        String t =  temperature.nextLine();
+
+        int temp = Integer.parseInt(t);
+
+        if (w == "Snow"|| temp < 12) {
+            System.out.println("Suggest you wear a water proof winter coat ");
+        } else if (w == "Rain" || w == "Cloudy" || 12 < temp && temp < 18) {
+            System.out.println("Suggest you wear a rain coat");
+        } else {
+            System.out.println("No need to wear a coat");
+        }
+
+
+
+
 
     }
 
